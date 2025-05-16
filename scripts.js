034e8ac7e7764324ae31ce0f67440ef969362155
Jhone Dias <jhonediasmd@gmail.com>
2025-05-15 22:59:02 -0300

    Update scripts.js
    jquery to auto fill phone code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-
+const countriesCodes = {
+    '' : '',
+    'br' : '+55',
+    'ca' : '+1',
+    'uk' : '+44'
+};
+$('#selectCountry').on('change', function(){
+    let selectedCode = countriesCodes[$(this).val()];
+    $('#registerPhoneCode').val(selectedCode);
+});
