110b3f97b21c8f11bf475ad688f190748f60303d
Jhone Dias <jhonediasmd@gmail.com>
2025-05-15 23:49:19 -0300

    Update scripts.js
    Script for email validation

--- scripts.js
+++ scripts.js
@@ -8,3 +8,17 @@ $('#selectCountry').on('change', function(){
     let selectedCode = countriesCodes[$(this).val()];
     $('#registerPhoneCode').val(selectedCode);
 });
+
+$("#registerForm").submit(function (e) { 
+    e.preventDefault();
+
+    let email = $("#registerEmail").val().trim();
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!regex.test(email)) {
+        $("#emailError").removeClass("d-none");
+        return;
+    } else {
+        $("#emailError").addClass("d-none");
+    }
+});
